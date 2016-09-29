4b6a2c3901cec28d54f6657f72c12deda00fb2e3
Tyson Gern <tgern@pivotal.io>
2016-09-29 11:02:39 -0600

    Initial commit
    Signed-off-by: Georg Apitz <gapitz@pivotal.io>

---
+++ src/main/kotlin/io/pivotal/DoneApplication.kt
@@ -0,0 +1,11 @@
+package io.pivotal
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+open class DoneApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(DoneApplication::class.java, *args)
+}
